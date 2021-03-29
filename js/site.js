54505e860882f67745e518c170b3c06c83282522
Benedek Korody <korody.benedek@gmail.com>
2021-03-30 00:01:01 +0200

    1

--- js/site.js
+++ js/site.js
@@ -6,14 +6,31 @@ var Site = {
         $('.cvv').on('blur', function () {
             Site.CardRotate(false);
         });
+
+        // todo F5 -re CardUpdate ha eltér defaulttól
+        // todo digits, date
+
+        $('#bank-card-owner').on('keyup', function () {
+           Site.CardUpdate('name', $(this).val());
+        });
+        $('#bank-card-cvv').on('keyup', function () {
+            Site.CardUpdate('cvv', $(this).val());
+        });
     },
     CardRotate: function (set) {
         $('.flip-card').toggleClass('turn', set);
     },
     CardUpdate: function (type, data) {
-
+        // todo digits, date
+        if (data == '') {
+            $('#' + type).text($('#' + type).data('default'));
+        } else {
+            $('#' + type).text(data);
+        }
     }
 };
+
+// todo submit loader
 
 $(window).on('load', function(){
     Site.Setup();
